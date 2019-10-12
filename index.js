1c802cc41a43310ea4ea343a8481036b22a3ab70
trungkien110298 <44560514+trungkien110298@users.noreply.github.com>
2019-10-12 16:09:01 +0700

    Init commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Rerquire Node JS module
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+
+
+
+// Creat server app
+var app = express();
+
+// View engine setup
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', function(req, res){
+    return res.render('view_op_worker.ejs')
+});
+
+
+
+// var upload = multer({ storage: storage }).single('uploadfile');
+// app.use(favicon(__dirname + '/public/image/favicon.ico'));
+
+
+
+app.listen(process.env.PORT || 3000, () => console.log('Server is listenning in port 3000'));
+module.exports = app;
