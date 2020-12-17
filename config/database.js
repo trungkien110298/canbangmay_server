a84c6caa0920cdc5760d567beae4532281486c40
Phan Trung Kiên <trungkien.it.98@gmail.com>
2020-12-17 21:57:41 +0700

    Database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secret': 'nodeauthsecret',
-  'database': 'mongodb://trungkien1102:Rde7tDuf3wsyP!Z@ds117719.mlab.com:17719/albs'
+  'database': 'mongodb+srv://trungkien1102:NXZolALOGpoRgglV@cluster0.uwwpg.mongodb.net/albs?retryWrites=true&w=majority'
 };
